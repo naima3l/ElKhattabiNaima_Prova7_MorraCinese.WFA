b794063a4e20098ca5449338f82dc7de0cf68042
naima.el.khattabi <naima.el.khattabi@avanade.com>
2021-09-10 15:02:51 +0200

    Add project files.

---
+++ ElKhattabiNaima_Prova7_MorraCinese.WFA/MorraCinese.Designer.cs
@@ -0,0 +1,220 @@
+﻿
+namespace ElKhattabiNaima_Prova7_MorraCinese.WFA
+{
+    partial class MorraCinese
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSasso = new System.Windows.Forms.Button();
+            this.btnForbici = new System.Windows.Forms.Button();
+            this.btnCarta = new System.Windows.Forms.Button();
+            this.btnConferma = new System.Windows.Forms.Button();
+            this.txtValue = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureUser = new System.Windows.Forms.PictureBox();
+            this.pictureComputer = new System.Windows.Forms.PictureBox();
+            this.textBoxComputer = new System.Windows.Forms.TextBox();
+            this.textBoxUser = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureUser)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureComputer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSasso
+            // 
+            this.btnSasso.Location = new System.Drawing.Point(26, 61);
+            this.btnSasso.Name = "btnSasso";
+            this.btnSasso.Size = new System.Drawing.Size(131, 34);
+            this.btnSasso.TabIndex = 0;
+            this.btnSasso.Text = "Sasso";
+            this.btnSasso.UseVisualStyleBackColor = true;
+            this.btnSasso.Click += new System.EventHandler(this.btnSasso_Click);
+            // 
+            // btnForbici
+            // 
+            this.btnForbici.Location = new System.Drawing.Point(26, 165);
+            this.btnForbici.Name = "btnForbici";
+            this.btnForbici.Size = new System.Drawing.Size(131, 34);
+            this.btnForbici.TabIndex = 1;
+            this.btnForbici.Text = "Forbici";
+            this.btnForbici.UseVisualStyleBackColor = true;
+            this.btnForbici.Click += new System.EventHandler(this.btnForbici_Click);
+            // 
+            // btnCarta
+            // 
+            this.btnCarta.Location = new System.Drawing.Point(26, 274);
+            this.btnCarta.Name = "btnCarta";
+            this.btnCarta.Size = new System.Drawing.Size(131, 34);
+            this.btnCarta.TabIndex = 2;
+            this.btnCarta.Text = "Carta";
+            this.btnCarta.UseVisualStyleBackColor = true;
+            this.btnCarta.Click += new System.EventHandler(this.btnCarta_Click);
+            // 
+            // btnConferma
+            // 
+            this.btnConferma.Location = new System.Drawing.Point(26, 383);
+            this.btnConferma.Name = "btnConferma";
+            this.btnConferma.Size = new System.Drawing.Size(131, 34);
+            this.btnConferma.TabIndex = 3;
+            this.btnConferma.Text = "Conferma";
+            this.btnConferma.UseVisualStyleBackColor = true;
+            this.btnConferma.Click += new System.EventHandler(this.btnConferma_Click);
+            // 
+            // txtValue
+            // 
+            this.txtValue.Location = new System.Drawing.Point(378, 383);
+            this.txtValue.Name = "txtValue";
+            this.txtValue.ReadOnly = true;
+            this.txtValue.Size = new System.Drawing.Size(274, 31);
+            this.txtValue.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(460, 355);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Esito Giocata";
+            // 
+            // pictureUser
+            // 
+            this.pictureUser.Image = global::ElKhattabiNaima_Prova7_MorraCinese.WFA.Properties.Resources.gingerbread_162141_1280;
+            this.pictureUser.Location = new System.Drawing.Point(292, 12);
+            this.pictureUser.Name = "pictureUser";
+            this.pictureUser.Size = new System.Drawing.Size(122, 103);
+            this.pictureUser.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureUser.TabIndex = 6;
+            this.pictureUser.TabStop = false;
+            // 
+            // pictureComputer
+            // 
+            this.pictureComputer.Image = global::ElKhattabiNaima_Prova7_MorraCinese.WFA.Properties.Resources._79176765_pc_cartoon_smiley_vector_icon_illustration_graphic_illustration;
+            this.pictureComputer.Location = new System.Drawing.Point(623, 12);
+            this.pictureComputer.Name = "pictureComputer";
+            this.pictureComputer.Size = new System.Drawing.Size(122, 103);
+            this.pictureComputer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureComputer.TabIndex = 7;
+            this.pictureComputer.TabStop = false;
+            // 
+            // textBoxComputer
+            // 
+            this.textBoxComputer.Location = new System.Drawing.Point(613, 137);
+            this.textBoxComputer.Name = "textBoxComputer";
+            this.textBoxComputer.ReadOnly = true;
+            this.textBoxComputer.Size = new System.Drawing.Size(154, 31);
+            this.textBoxComputer.TabIndex = 8;
+            // 
+            // textBoxUser
+            // 
+            this.textBoxUser.Location = new System.Drawing.Point(273, 137);
+            this.textBoxUser.Name = "textBoxUser";
+            this.textBoxUser.ReadOnly = true;
+            this.textBoxUser.Size = new System.Drawing.Size(154, 31);
+            this.textBoxUser.TabIndex = 9;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ElKhattabiNaima_Prova7_MorraCinese.WFA.Properties.Resources.detective_1424831_1280;
+            this.pictureBox1.Location = new System.Drawing.Point(292, 206);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(122, 126);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::ElKhattabiNaima_Prova7_MorraCinese.WFA.Properties.Resources.detective_1424831_1280;
+            this.pictureBox2.Location = new System.Drawing.Point(623, 206);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(122, 126);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 11;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label2.Location = new System.Drawing.Point(482, 259);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 25);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "VS";
+            // 
+            // MorraCinese
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBoxUser);
+            this.Controls.Add(this.textBoxComputer);
+            this.Controls.Add(this.pictureComputer);
+            this.Controls.Add(this.pictureUser);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtValue);
+            this.Controls.Add(this.btnConferma);
+            this.Controls.Add(this.btnCarta);
+            this.Controls.Add(this.btnForbici);
+            this.Controls.Add(this.btnSasso);
+            this.Name = "MorraCinese";
+            this.Text = "Morra Cinese";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureUser)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureComputer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSasso;
+        private System.Windows.Forms.Button btnForbici;
+        private System.Windows.Forms.Button btnCarta;
+        private System.Windows.Forms.Button btnConferma;
+        private System.Windows.Forms.TextBox txtValue;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureUser;
+        private System.Windows.Forms.PictureBox pictureComputer;
+        private System.Windows.Forms.TextBox textBoxComputer;
+        private System.Windows.Forms.TextBox textBoxUser;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ElKhattabiNaima_Prova7_MorraCinese.WFA/MorraCinese.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ElKhattabiNaima_Prova7_MorraCinese.WFA
+{
+    public partial class MorraCinese : Form
+    {
+        int user, pc, count=0, countUser=0,countPc=0;
+        public MorraCinese()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSasso_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = global::ElKhattabiNaima_Prova7_MorraCinese.WFA.Properties.Resources.sasso;
+            user = 1;
+            txtValue.Clear();
+        }
+
+        private void btnForbici_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = global::ElKhattabiNaima_Prova7_MorraCinese.WFA.Properties.Resources.forbice;
+            user = 2;
+            txtValue.Clear();
+        }
+
+        private void btnCarta_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = global::ElKhattabiNaima_Prova7_MorraCinese.WFA.Properties.Resources.carta;
+            user = 3;
+            txtValue.Clear();
+        }
+
+
+        private void btnConferma_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            int choice = random.Next(1, 3);
+            switch (choice)
+            {
+                case 1:
+                    pictureBox2.Image = global::ElKhattabiNaima_Prova7_MorraCinese.WFA.Properties.Resources.sasso;
+                    pc = 1;
+                    txtValue.Clear();
+                    break;
+                case 2:
+                    pictureBox2.Image = global::ElKhattabiNaima_Prova7_MorraCinese.WFA.Properties.Resources.forbice;
+                    pc = 2;
+                    txtValue.Clear();
+                    break;
+                case 3:
+                    pictureBox2.Image = global::ElKhattabiNaima_Prova7_MorraCinese.WFA.Properties.Resources.carta;
+                    pc = 3;
+                    txtValue.Clear();
+                    break;
+            }
+
+            if (user == pc)
+            {
+                txtValue.Text = "parità";
+            }
+            else if (user == 1 && pc == 2 || user == 1 && pc == 3 || user == 3 && pc == 1)
+            {
+                countUser += 1;
+                textBoxUser.Text = $"{countUser}";
+                if ((countUser + countPc == 3) && countUser > countPc)
+                {
+                    txtValue.Text = $"Hai vinto {countUser} a {countPc} :) !";
+                    count = 0;
+                    countPc = 0;
+                    countUser = 0;
+                    textBoxComputer.Clear();
+                    textBoxUser.Clear();
+                    return;
+                }
+                else if ((countUser + countPc == 3) && countUser < countPc)
+                {
+                    txtValue.Text = $"Hai perso {countUser} a {countPc} :( !";
+                    count = 0;
+                    countPc = 0;
+                    countUser = 0;
+                    textBoxComputer.Clear();
+                    textBoxUser.Clear();
+                    return;
+                }
+                count++;
+            }
+            else
+            {
+                countPc += 1;
+                textBoxComputer.Text = $"{countPc}";
+                if ((countUser + countPc == 3) && countUser > countPc)
+                {
+                    txtValue.Text = $"Hai vinto {countUser} a {countPc} :) !";
+                    count = 0;
+                    countPc = 0;
+                    countUser = 0;
+                    textBoxComputer.Clear();
+                    textBoxUser.Clear();
+                    return;
+                }
+                else if ((countUser + countPc == 3) && countUser < countPc)
+                {
+                    txtValue.Text = $"Hai perso {countUser} a {countPc} :( !";
+                    count = 0;
+                    countPc = 0;
+                    countUser = 0;
+                    textBoxComputer.Clear();
+                    textBoxUser.Clear();
+                    return;
+                }
+                count++;
+            }
+            //pictureBox1.Image = global::ElKhattabiNaima_Prova7_MorraCinese.WFA.Properties.Resources.detective_1424831_1280;
+            //pictureBox2.Image = global::ElKhattabiNaima_Prova7_MorraCinese.WFA.Properties.Resources.detective_1424831_1280;
+        }
+    }
+}
